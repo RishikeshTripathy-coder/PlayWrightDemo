5c2001b87731294dbff5cec8ea2db83896ca2209
skybiet89 <skybiet89@gmail.com>
2024-04-14 12:29:49 +0530

    both assigment done

--- tests-examples/task2.spec.ts
+++ tests-examples/task2.spec.ts
@@ -12,7 +12,8 @@ test('Opening',async()=>{
 test('Register without agree',async()=>{
     const br:Browser=await firefox.launch({headless:false});
     const page:Page = await br.newPage();
-    await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
+    await page.goto(' https://ecommerce-playground.lambdatest.io/index.php?route=common/home');
+    await page.locator('//div[@id="entry_217834"]//span[@class="title" and contains(text(),"My account")]').click();
     await page.locator('//a[@class="btn btn-primary"]').click();
     await page.locator('//input[@id="input-firstname"]').fill("Sumit");
     await page.locator('id=input-lastname').fill("Yadav");
@@ -23,7 +24,7 @@ test('Register without agree',async()=>{
     await page.locator('//input[@id="input-confirm"]').fill("India@123");
     // await page.locator('//label[@class="custom-control-label" and @for="input-agree"]').click();
     await page.locator('//input[@class="btn btn-primary"]').click();
-    await page.locator('//div[@class="alert alert-danger alert-dismissible"]').isVisible();
+    await page.locator('//div[@class="alert alert-danger alert-dismissible"]').isEnabled()==true;
     
     
 
@@ -35,7 +36,8 @@ test('Register without agree',async()=>{
 test('Register',async()=>{
     const br:Browser=await firefox.launch({headless:false});
     const page:Page = await br.newPage();
-    await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
+    await page.goto(' https://ecommerce-playground.lambdatest.io/index.php?route=common/home');
+    await page.locator('//div[@id="entry_217834"]//span[@class="title" and contains(text(),"My account")]').click();
     await page.locator('//a[@class="btn btn-primary"]').click();
     await page.locator('//input[@id="input-firstname"]').fill("Sumit");
     await page.locator('id=input-lastname').fill("Yadav");
